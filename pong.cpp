7e4245ebb4d1f5f1773f8103ac38fa16956197a9
scunn <shanecunn@hotmail.com>
2022-10-08 17:11:03 +0100

    Left, Right paddles drawn

--- pong.cpp
+++ pong.cpp
@@ -10,6 +10,12 @@ int main(){
             ClearBackground(BLACK);
             // Draw circle, Get screen width + height & divide by 2. Set Radius at 5, Colour object WHITE
             DrawCircle(GetScreenWidth() / 2, GetScreenHeight() / 2, 5, WHITE);
+            // Draw Left Paddle Rectangle, Subtract half of screen height (2 - 50) to center paddle position on Y axis
+            DrawRectangle(50, GetScreenHeight() / 2 - 50, 10, 100, WHITE);
+            // Draw Right Paddle Rectangle, Subtract half of screen height (2 - 50) to center paddle position on Y axis
+            // DrawRectangle(740, GetScreenHeight() /2 - 50, 10, 100, WHITE);
+            // Draw Right Paddle Rectangle, Get screen width subtract by 50 and 10 
+            DrawRectangle(GetScreenWidth() - 50 - 10, GetScreenHeight() / 2 - 50, 10, 100, WHITE);
             // Draw Frames per second
             DrawFPS(10, 10);
         EndDrawing();
